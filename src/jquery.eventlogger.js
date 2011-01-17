a5a1d6c7b97a5b40760e60f39649d3f1805f42fd
bondehagen <anders@bondehagen.com>
2011-01-17 10:36:19 +0100

    First commit

---
+++ src/jquery.eventlogger.js
@@ -0,0 +1,70 @@
+(function() {
+
+   var lastEventTime = new Date();
+   var jQueryHandle = jQuery.event.handle;
+   var jQueryAdd = jQuery.event.add;
+
+   HTMLElement.prototype.html = function () {
+      var a = this.attributes, str = "<" + this.tagName.toLowerCase();
+      for (var i in a) if (a[i].specified)
+         str += " " + a[i].name + '="' + a[i].value + '"';
+      return str + " />";
+   };
+
+   function formatTime(time) {
+      return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + ":" + time.getMilliseconds();
+   }
+
+   jQuery.event.add = function(elem, types, handler, data) {
+      // Execute the original jQuery method.
+      var ret = jQueryAdd.apply(this, arguments);
+
+      console.groupCollapsed("Bind event: " + types);
+      console.log("add: " + types + " elem " + elem.html());
+      console.log(elem);
+      console.log(handler.toSource());
+      //console.trace();
+      console.groupEnd();
+      return ret;
+   };
+
+   jQuery.event.handle = function(event) {
+      var currentTime = new Date();
+      if ((currentTime - lastEventTime) > 500)
+         console.log("________");
+      lastEventTime = currentTime;
+
+      console.groupCollapsed("Event: " + event.type);
+      var elem = event.target;
+      console.log(event.type + " " + elem.html() + " " + formatTime(currentTime));
+      /*if (event.timeStamp) {
+       console.groupCollapsed("Trace");
+       console.trace();
+       console.groupEnd();
+       }*/
+
+      var events = jQuery.data(this, this.nodeType ? "events" : "__events__");
+
+      if (typeof events === "function") {
+         events = events.events;
+      }
+
+      var handlers = (events || {})[ event.type ];
+
+      if (events && handlers) {
+         // Clone the handlers to prevent manipulation
+         handlers = handlers.slice(0);
+
+         for (var j = 0, l = handlers.length; j < l; j++) {
+            console.log(handlers[j].handler.toSource());
+         }
+      }
+
+      console.groupEnd();
+
+      // Execute the original jQuery method.
+      var ret = jQueryHandle.apply(this, arguments);
+      return ret;
+   };
+
+})();
